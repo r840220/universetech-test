73013b5e869b6807de3215532e429fff3a84a330
馬晟榮 <>
2021-03-20 17:54:49 +0800

    [feat] 解決n+1 query

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TopicResource extends JsonResource
 {
+	public static function collection($resource)
+	{
+		$resource->loadMissing('comments');
+
+		return parent::collection($resource);
+	}
+
     public function toArray()
     {
         return [
